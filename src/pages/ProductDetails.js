e6b6e4fa69e007ae7858eb7924f75ca429eeb1de
rahithya teja <ambarapurahithya@gmail.com>
2022-07-06 13:03:14 +0530

    RouterBasics(react)

---
+++ src/pages/ProductDetails.js
@@ -0,0 +1,15 @@
+import { useParams } from "react-router-dom";
+const ProductDetails = ()=>{
+
+    const params = useParams();
+
+
+    return(
+        <section>
+            <h1>ProductDetails</h1>
+            <p>{params.productId}</p>
+        </section>
+    );
+};
+
+export default ProductDetails;
